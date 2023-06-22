4fbf58af80ef8ebb006de51587af782f7810d7b5
Charles Teague <cteague@gmail.com>
2023-06-22 19:05:59 -0400

    Remove - in python name

--- src/__tests__/jupyterlab_quarto.spec.ts
+++ src/__tests__/jupyterlab_quarto.spec.ts
@@ -2,7 +2,7 @@
  * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
  */
 
-describe('jupyterlab-quarto', () => {
+describe('@quarto/jupyterlab-quarto', () => {
   it('should be tested', () => {
     expect(1 + 1).toEqual(2);
   });
