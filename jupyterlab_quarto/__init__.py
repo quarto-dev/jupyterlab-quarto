3b112f8a4b2c3caa9bdc4b11ebced9d317ca0982
Charles Teague <cteague@gmail.com>
2023-06-22 09:53:01 -0400

    try correcting paths

--- jupyterlab_quarto/__init__.py
+++ jupyterlab_quarto/__init__.py
@@ -12,5 +12,5 @@ except ImportError:
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": "jupyterlab-quarto"
+        "dest": "jupyterlab_quarto"
     }]
