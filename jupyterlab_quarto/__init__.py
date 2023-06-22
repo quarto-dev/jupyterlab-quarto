5192e51b700e67c1148d9361c028d574b90318ee
Charles Teague <cteague@gmail.com>
2023-06-22 11:07:44 -0400

    try omitting the organization

--- jupyterlab_quarto/__init__.py
+++ jupyterlab_quarto/__init__.py
@@ -12,5 +12,5 @@ except ImportError:
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": "@quarto/jupyterlab-quarto"
+        "dest": "jupyterlab-quarto"
     }]
