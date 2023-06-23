4eb021756e1c3a47fbbaec53763f9aeaeeade6c0
Charles Teague <cteague@gmail.com>
2023-06-23 11:41:50 -0400

    Revert "Rename npm extension"
    This reverts commit 5edb43ba1dd0ba9461ac7ba2a5439f96ff3ac5b9.

--- jupyterlab_quarto/__init__.py
+++ jupyterlab_quarto/__init__.py
@@ -12,5 +12,5 @@ except ImportError:
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": "jupyterlab_quarto"
+        "dest": "@quarto/jupyterlab-quarto"
     }]
