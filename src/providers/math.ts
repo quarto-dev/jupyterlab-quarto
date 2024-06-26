02c2d3a1701f6f143685158ca2cd1fb0b7fe90e7
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2024-06-26 12:26:32 -0700

    no polyfill.io

--- src/providers/math.ts
+++ src/providers/math.ts
@@ -95,7 +95,7 @@ MathJax = {
           const polyFillEl = document.createElement('script');
           polyFillEl.setAttribute(
             'src',
-            'https://polyfill.io/v3/polyfill.min.js?features=es6'
+            'https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?features=es6'
           );
           document.head.appendChild(polyFillEl);
 
@@ -115,7 +115,7 @@ MathJax = {
 });
 
 /*
-<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+<script src="https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
 */
